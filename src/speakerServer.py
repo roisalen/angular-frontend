0430dd49805b6406d1a16672eb1d6b7dcccb9ce0
torkilv <torkilv@gmail.com>
2013-10-20 14:54:59 +0200

    Added simple getting and posting of speakerobjects, no error handling

--- src/speakerServer.py
+++ src/speakerServer.py
@@ -1,5 +1,7 @@
 import os
-from bottle import route, run
+import json
+from bottle import route, run, request
+from store import redis
 
 
 class Speaker:
@@ -17,8 +19,27 @@ class Organization:
 		this.name = name
 		this.meetings = {}
 
-@route('/')
-def taler_add():
-	return "Hello world"
+@route('/<speakerId>', method="GET")
+def get_speaker(speakerId):
+	return redis.get(speakerId)
+
+@route('/<speakerId>', method="POST")
+def post_speaker(speakerId):
+	data = request.body.readline()
+	entity = json.loads(data)
+	redis.set(speakerId, entity)
+
+
+@route('/<speakerId>', method="PUT")
+def update_speaker(speakerId):
+	data = request.body.readline()
+	entity = json.loads(data)
+	existingObject = redis.get(speakerId)
+	existingObject.update(entity)
+	redis.set(speakerId, existingObject)
+
+
+
+
 
 run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
