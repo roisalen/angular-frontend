0b7370ee7b9e1832900e8a8fb60907fe615f662b
torkilv <torkilv@gmail.com>
2013-11-30 20:51:43 +0100

    SpeakerRepository now an object

--- scripts/set-speaker-names.js
+++ scripts/set-speaker-names.js
@@ -1,4 +1,5 @@
 speaker = new Speaker("Torkil", 1);
+speakerRepository = new SpeakerRepository();
 speakerRepository.add(speaker);
 console.log(speakerRepository.getSpeakers());
 console.log(speakerRepository.size())
