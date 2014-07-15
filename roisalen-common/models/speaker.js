480a3d32aa6cbe11b30ebf7c6e38b69449ef65a8
torkilv <torkilv@gmail.com>
2014-07-15 18:34:55 +0200

    _id should be number to ensure uniqueness

--- roisalen-common/models/speaker.js
+++ roisalen-common/models/speaker.js
@@ -4,6 +4,7 @@ module.exports = function(name, number, sex, group) {
 	console.log(name);
 	this.name = name;
 	this.number = number;
+	this._id = number;
 	this.sex = sex;
 	this.group = group;
 	this.replies = new SpeakerQueue();
